e761f18cc528e21698972ad48b47c1945a8088c0
wembleyWilliams <wembleywilliamsdev@gmail.com>
2024-08-22 15:54:13 -0500

    Updated versioning config. New base version number in package.json was manually written to match release version 0.1.1

--- update-version.js
+++ update-version.js
@@ -7,6 +7,7 @@ const versionFilePath = path.join(__dirname, 'version.txt');
 
 if (version) {
     fs.writeFileSync(versionFilePath, version, 'utf8');
+
     console.log(`Version ${version} written to version.txt`);
 } else {
     console.error('Version number is missing in package.json');
