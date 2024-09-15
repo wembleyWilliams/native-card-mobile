32db4bd5b83f490a03fa5762306d7b429cedfa40
wembleyWilliams <wembleywilliamsdev@gmail.com>
2024-09-15 13:50:07 -0500

    chore: Modified new webpack config and metro config to export the expo app into singular web bundle which will be under the dist folder

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+let createWebpackConfigAsync  = require('@expo/webpack-config');
+let path = require('path');
+
+module.exports = async function (env, argv) {
+    const config = await createWebpackConfigAsync(env, argv);
+    config.resolve.alias['react-native'] = 'react-native-web';
+    config.resolve.alias['react-native-maps'] = 'react-native-web-maps';
+    config.target = 'web';
+    config.mode = 'development';
+
+    config.output = {
+        path: path.resolve(__dirname, 'web-build'),
+        filename: 'bundle.js',
+    };
+
+    config.optimization = {
+        splitChunks: false,
+        runtimeChunk: false,
+    };
+
+
+    config.resolve = {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    };
+
+    config.module.rules.push(
+        {
+            test: /\.(ts)x?$/,
+            exclude: /node_modules|\.d\.ts$/, // this line as well
+            use: {
+                loader: 'ts-loader',
+                options: {
+                    compilerOptions: {
+                        noEmit: false, // this option will solve the issue
+                    },
+                },
+            },
+        },
+        {
+            test: /\.js$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+                },
+            },
+            exclude: /node_modules/,
+        }
+    );
+
+    config.devtool = 'source-map';
+
+    return config;
+};
